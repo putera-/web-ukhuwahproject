09d17dd6722726220bafb92a8e5c8bc6337c9d3d
Rizjami Putera <rizjamiputera@gmail.com>
2024-03-30 17:24:22 +0700

    fix client validation

--- utils/client-validation.ts
+++ utils/client-validation.ts
@@ -4,8 +4,8 @@ export const isUpdateclient = Joi.object({
     nick: isString.min(1).max(20).required().label("Singkatan"),
     slogan: isString.allow(null, '').max(100).label("Slogan"),
     phone: isPhone.allow(null, '').label("No Telpon"),
-    youtube: isURLText.allow(null, '').label("Youtube"),
-    instagram: isURLText.allow(null, '').label("Instagram"),
-    facebook: isURLText.allow(null, '').label("Facebook"),
-    twitter: isURLText.allow(null, '').label("Twitter")
+    youtube: Joi.string().trim().max(100).allow(null, '').label("Youtube"),
+    instagram: Joi.string().trim().max(100).allow(null, '').label("Instagram"),
+    facebook: Joi.string().trim().max(100).allow(null, '').label("Facebook"),
+    twitter: Joi.string().trim().max(100).allow(null, '').label("Twitter")
 });
