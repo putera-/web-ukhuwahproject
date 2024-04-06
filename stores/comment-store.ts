4348820659f29cd1a2383e450212d77f81682c76
Rizjami Putera <rizjamiputera@gmail.com>
2024-04-06 11:28:29 +0700

    fix calculate count

--- stores/comment-store.ts
+++ stores/comment-store.ts
@@ -20,6 +20,7 @@ export const useCommentStore = defineStore('use-comment', {
             const new_comment: Comment = await Api.post('/comments/itikaf/' + Itikaf.itikaf.id, { comment }) as Comment;
 
             if (Itikaf.itikaf) {
+                ++Itikaf.itikaf._count!.comments!;
                 if (Itikaf.itikaf.comments) Itikaf.itikaf.comments.unshift(new_comment);
             }
         },
@@ -33,6 +34,7 @@ export const useCommentStore = defineStore('use-comment', {
 
             const schedule = Itikaf.schedules.find(s => s.id == scheduleId);
             if (schedule) {
+                ++schedule._count!.comments!;
                 if (schedule.comments) schedule.comments.unshift(new_comment);
             }
         },
@@ -44,13 +46,13 @@ export const useCommentStore = defineStore('use-comment', {
 
             const new_comment_reply: CommentReply = await Api.post('/comments/reply/' + commentId, { comment }) as CommentReply;
 
-            if (Itikaf.itikaf) {
-                if (Itikaf.itikaf.comments) {
-                    const _comment = Itikaf.itikaf.comments.find(c => c.id == commentId);
+            if (!Itikaf.itikaf) return;
+            if (!Itikaf.itikaf.comments) return;
 
-                    if (_comment.replies) _comment.replies.unshift(new_comment_reply);
-                }
-            }
+            const _comment = Itikaf.itikaf.comments.find((c: Comment) => c.id == commentId);
+
+            ++_comment._count!.replies!;
+            if (_comment.replies) _comment.replies.unshift(new_comment_reply);
         },
         async replyItikafScheduleComment(scheduleId: string, commentId: string, comment: string): Promise<void> {
             const Api = useApiStore();
@@ -61,13 +63,13 @@ export const useCommentStore = defineStore('use-comment', {
             const new_comment_reply: CommentReply = await Api.post('/comments/reply/' + commentId, { comment }) as CommentReply;
 
             const schedule = Itikaf.schedules.find(s => s.id == scheduleId);
-            if (schedule) {
-                if (schedule.comments) {
-                    const _comment = schedule.comments.find(c => c.id == commentId);
+            if (!schedule) return;
+            if (!schedule.comments) return;
+            const _comment = schedule.comments.find((c: Comment) => c.id == commentId);
+            if (!_comment) return;
 
-                    if (_comment?.replies) _comment.replies.unshift(new_comment_reply);
-                }
-            }
+            ++_comment._count!.replies!;
+            if (_comment?.replies) _comment.replies.unshift(new_comment_reply);
         },
         async removeItikafComment(id: string): Promise<void> {
             const Api = useApiStore();
@@ -78,7 +80,8 @@ export const useCommentStore = defineStore('use-comment', {
             if (!Itikaf.itikaf) return;
             if (!Itikaf.itikaf.comments) return;
 
-            const index = Itikaf.itikaf.comments.findIndex(c => c.id == id);
+            --Itikaf.itikaf._count!.comments!;
+            const index = Itikaf.itikaf.comments.findIndex((c: Comment) => c.id == id);
             if (index != -1) {
                 Itikaf.itikaf.comments.splice(index, 1);
             }
@@ -96,6 +99,7 @@ export const useCommentStore = defineStore('use-comment', {
             if (!comment) return;
             if (!comment.replies) return;
 
+            --comment._count!.replies;
             const index = comment.replies.findIndex((r: CommentReply) => r.id == id);
             if (index != -1) {
                 comment.replies.splice(index, 1);
@@ -108,14 +112,12 @@ export const useCommentStore = defineStore('use-comment', {
             await Api.delete('/comments/' + id);
 
             const schedule = Itikaf.schedules.find(s => s.id == scheduleId);
-            console.log(schedule);
 
             if (!schedule) return;
-            console.log(schedule.comments);
             if (!schedule.comments) return;
 
+            --schedule._count!.comments!;
             const index = schedule.comments.findIndex(c => c.id == id);
-            console.log(index);
             if (index != -1) {
                 schedule.comments.splice(index, 1);
             }
@@ -135,6 +137,7 @@ export const useCommentStore = defineStore('use-comment', {
             if (!comment) return;
             if (!comment.replies) return;
 
+            --comment._count!.replies!;
             const index = comment.replies.findIndex((r: CommentReply) => r.id == id);
             if (index != -1) {
                 comment.replies.splice(index, 1);
