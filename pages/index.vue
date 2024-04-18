09180e21a74e8c6d305611739025570c6cb857d9
Rizjami Putera <rizjamiputera@gmail.com>
2024-04-18 21:27:58 +0700

    - Fix client only - Fix shalat schedule

--- pages/index.vue
+++ pages/index.vue
@@ -20,29 +20,31 @@
     <div class="max-w-7xl w-full mx-auto md:pt-40 p-3 md:p-10 overflow-hidden">
 
         <!-- ITIKAF -->
-        <div v-if="Itikaf.itikaf">
-            <div class="text-xl md:text-2xl font-semibold my-3">
-                I'tikaf Ramadhan {{ Itikaf.itikaf.year }} / {{ Itikaf.itikaf.hijri_year }} H
-            </div>
-            <!-- schedule.photos -->
-            <div v-if="Itikaf.itikaf.photo"
-                class="w-full h-[30vh] md:h-[50vh] lg:h-[70vh] rounded-2xl overflow-hidden relative bg-cover bg-center shadow-xl"
-                :style="`background-image: url(${isURL(Itikaf.itikaf.photo) ? Itikaf.itikaf.photo : apiUri + Itikaf.itikaf.photo});`">
-                <!-- <div class="w-full h-full bg-gradient-to-t from-white via-white/0 to-white/0 absolute"></div> -->
-                <!-- <img v-if="isURL(Itikaf.itikaf.photo)" :src="Itikaf.itikaf.photo" class="w-full z-10">
-                <img v-else :src="apiUri + Itikaf.itikaf.photo" class="w-full z-10"> -->
-            </div>
-            <div v-else class="w-full h-40 md:h-52 lg:h-60 rounded-t-xl overflow-hidden relative">
-                <div class="w-full h-full bg-gradient-to-t from-white to-[#F8D7B6] absolute"></div>
-            </div>
+        <ClientOnly>
+            <div v-if="Itikaf.itikaf">
+                <div class="text-xl md:text-2xl font-semibold my-3">
+                    I'tikaf Ramadhan {{ Itikaf.itikaf.year }} / {{ Itikaf.itikaf.hijri_year }} H
+                </div>
+                <!-- schedule.photos -->
+                <div v-if="Itikaf.itikaf.photo"
+                    class="w-full h-[30vh] md:h-[50vh] lg:h-[70vh] rounded-2xl overflow-hidden relative bg-cover bg-center shadow-xl"
+                    :style="`background-image: url(${isURL(Itikaf.itikaf.photo) ? Itikaf.itikaf.photo : apiUri + Itikaf.itikaf.photo});`">
+                    <!-- <div class="w-full h-full bg-gradient-to-t from-white via-white/0 to-white/0 absolute"></div> -->
+                    <!-- <img v-if="isURL(Itikaf.itikaf.photo)" :src="Itikaf.itikaf.photo" class="w-full z-10">
+                    <img v-else :src="apiUri + Itikaf.itikaf.photo" class="w-full z-10"> -->
+                </div>
+                <div v-else class="w-full h-40 md:h-52 lg:h-60 rounded-t-xl overflow-hidden relative">
+                    <div class="w-full h-full bg-gradient-to-t from-white to-[#F8D7B6] absolute"></div>
+                </div>
 
-            <div class="mt-2">{{ Itikaf.itikaf.description }}</div>
-            <div class="my-3">
-                <div v-if="Itikaf.itikaf.masjid" class="font-semibold">Lokasi: {{ Itikaf.itikaf.masjid }}</div>
-                <div v-if="Itikaf.itikaf.address">Alamat: {{ Itikaf.itikaf.address }}</div>
+                <div class="mt-2">{{ Itikaf.itikaf.description }}</div>
+                <div class="my-3">
+                    <div v-if="Itikaf.itikaf.masjid" class="font-semibold">Lokasi: {{ Itikaf.itikaf.masjid }}</div>
+                    <div v-if="Itikaf.itikaf.address">Alamat: {{ Itikaf.itikaf.address }}</div>
+                </div>
+                <NuxtLink to="/itikaf" class="btn bg-[#EE9A49] mt-2 rounded-full">Lihat Jadwal I'tikaf</NuxtLink>
             </div>
-            <NuxtLink to="/itikaf" class="btn bg-[#EE9A49] mt-2 rounded-full">Lihat Jadwal I'tikaf</NuxtLink>
-        </div>
+        </ClientOnly>
     </div>
 </template>
 
@@ -55,7 +57,7 @@ const { public: { apiUri } } = useRuntimeConfig();
 const Client = useClientStore();
 const Itikaf = useItikafStore();
 const Shalat = useShalatStore()
-// if (!Shalat.data) Shalat.get();
+Shalat.get();
 await Itikaf.get();
 
 
--- stores/shalat-store.ts
+++ stores/shalat-store.ts
@@ -1,26 +1,35 @@
 import { Coordinates, CalculationMethod, PrayerTimes, Madhab } from 'adhan';
 
 interface ShalatState {
-    data?: PrayerTimes
+    fajr: string | Date
+    sunrise: string | Date
+    dhuhr: string | Date
+    asr: string | Date
+    maghrib: string | Date
+    isha: string | Date
 }
 export const useShalatStore = defineStore('shalat', {
     state: (): ShalatState => ({
-        data: undefined
+        fajr: new Date().toISOString(),
+        sunrise: new Date().toISOString(),
+        dhuhr: new Date().toISOString(),
+        asr: new Date().toISOString(),
+        maghrib: new Date().toISOString(),
+        isha: new Date().toISOString(),
     }),
-    getters: {
-        fajr: (state) => state.data ? state.data.fajr : '-',
-        sunrise: (state) => state.data ? state.data.sunrise : '-',
-        dhuhr: (state) => state.data ? state.data.dhuhr : '-',
-        asr: (state) => state.data ? state.data.asr : '-',
-        maghrib: (state) => state.data ? state.data.maghrib : '-',
-        isha: (state) => state.data ? state.data.isha : '-',
-    },
     actions: {
         async get(): Promise<void> {
             const coordinates = new Coordinates(-6.330386, 106.8060391);
             const params = CalculationMethod.Singapore();
             const date = new Date();
-            this.data = new PrayerTimes(coordinates, date, params);
+            const data = new PrayerTimes(coordinates, date, params);
+
+            this.fajr = data.fajr;
+            this.sunrise = data.sunrise;
+            this.dhuhr = data.dhuhr;
+            this.asr = data.asr;
+            this.maghrib = data.maghrib;
+            this.isha = data.isha;
         },
     }
 });
